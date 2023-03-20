bf1cfbcd404f2acf878c61460155a3d80a1a76c4
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-20 10:12:59 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [data, setData] = useState({});
+  console.log(data);
+  const [loading, setLoading] = useState(false);
+  const fetchData = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setData(data.results[0]);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const URL = "https://randomuser.me/api";
+    fetchData(URL);
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="user-image">
+          <img src={data.picture?.large} alt={data.name?.first} />
+        </div>
+        <div className="user-info">
+          <span className="user-email"></span>
+          <span className="user-tell"></span>
+          <span className="user-country"></span>
+        </div>
+      </div>
+    </div>
+  );
+}
